6524b20e857370cba5a65c2d59122ca0797f923e
joackagui <joaquinaguilerasalinas1@gmail.com>
2026-04-23 08:37:35 -0400

    proper scene loading and characters box collisions

--- Assets/Scripts/Batman.cs
+++ Assets/Scripts/Batman.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Batman : MonoBehaviour
+public class Batman : Character
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Scripts/Joker.cs
+++ Assets/Scripts/Joker.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Joker : MonoBehaviour
+public class Joker : Character
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
